8691f7c7693bb8c1f2253f1e4a7dc68baf73d84f
Steve Phillips <steve@tryingtobeawesome.com>
2018-02-13 07:47:28 -0800

    Added cryptagorg.service; updated run.sh with fancier rkt command

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo systemd-run --slice=machine rkt run --set-env=CADDYPATH=/.caddy --volume site,kind=host,source=`pwd` --volume dotcaddy,kind=host,source=/home/ubuntu/.caddy --insecure-options=image --net=host cryptagorg-linux-amd64.aci
+cd /home/ubuntu/code/checkouts/cryptagorg && \
+sudo systemd-run --slice=machine rkt run --set-env=CADDYPATH=/.caddy --volume site,kind=host,source=/home/ubuntu/code/checkouts/cryptagorg --volume dotcaddy,kind=host,source=/home/ubuntu/.caddy --insecure-options=image --net=host --volume=resolv,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=resolv,target=/etc/resolv.conf --volume cacert,kind=host,source=/etc/ssl/certs/ca-certificates.crt,readOnly=true --mount volume=cacert,target=/etc/ssl/certs/ca-certificates.crt /home/ubuntu/code/checkouts/cryptagorg/cryptagorg-linux-amd64.aci
